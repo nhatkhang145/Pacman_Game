15d26e993fcf583944297bf5e770ea219d73a62d
23130308-TinThanh <23130308@st.hcmuaf.edu.vn>
2026-05-20 12:13:16 +0700

    test_01

---
+++ src/main/java/com/example/pacmangame/SourceCode/SettingsManager.java
@@ -0,0 +1,31 @@
+package com.example.pacmangame;
+
+public class SettingsManager {
+    private static SettingsManager instance;
+
+    public enum Language {
+        VI, EN
+    }
+
+    private double volume = 1.0;
+    private boolean isMuted = false;
+    private Language language = Language.VI;
+
+    private SettingsManager() {}
+
+    public static SettingsManager getInstance() {
+        if (instance == null) {
+            instance = new SettingsManager();
+        }
+        return instance;
+    }
+
+    public double getVolume() { return volume; }
+    public void setVolume(double volume) { this.volume = volume; }
+    
+    public boolean isMuted() { return isMuted; }
+    public void setMuted(boolean muted) { this.isMuted = muted; }
+    
+    public Language getLanguage() { return language; }
+    public void setLanguage(Language language) { this.language = language; }
+}
